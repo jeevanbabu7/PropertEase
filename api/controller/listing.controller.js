f438559372117caa27f7aee9fc0298d63f168875
Jeevan Babu <jeevanbabu190@gmail.com>
2024-05-01 17:36:53 +0530

    complete search api

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -80,3 +80,41 @@ export const propertyDetails = async (req,res,next) => {
         next(err);
     }
 }
+
+export const getListing = async (req,res,next) => {
+
+    try {
+        const limit = parseInt(req.query.limit) || 10;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let furnished = req.query.furnished;
+        if(furnished == undefined || furnished == 'false') {
+            furnished = { $in: [false,true]}
+        }
+
+        let parking = req.query.parking;
+        if(parking == undefined || parking == 'false') {
+            parking = { $in: [false,true]}
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: {$regex: searchTerm,$options: 'i'},
+            furnished,
+            parking
+        }).sort(
+            {[sort]: order}
+        ).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings)
+
+        
+    }catch(err) {
+        next(err);
+    }
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing,deleteProperty, updateProperty,propertyDetails} from '../controller/listing.controller.js';
+import { createListing,deleteProperty, updateProperty,propertyDetails,getListing} from '../controller/listing.controller.js';
 const router = express.Router();
 
 router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteProperty);
 router.post('/update/:id',verifyToken,updateProperty) // update property data
 router.get('/property/:id',verifyToken,propertyDetails); // fetch info about single property
+router.get('/get',getListing);
 export default router;
